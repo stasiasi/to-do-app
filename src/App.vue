40d56d9e453a2e11103ea4296670308a36c63fa6
stasiasi <ad.simonovaa@gmail.com>
2024-06-19 14:53:03 +0300

    feat: implement the function for deleting tasks

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
-  <div></div>
+  <TodoItem :taskList="actualTasks" @DeleteTask="deleteTodo"></TodoItem>
+  <hr />
+  <TodoItem :taskList="completedTasks" @DeleteTask="deleteTodo"></TodoItem>
 </template>
 
 <script setup>
 import { getTodos } from './API/APITodos'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
+import TodoItem from './components/TodoItem.vue'
 
 const taskList = ref([])
 const userId = ref(1)
+
+const actualTasks = computed(() => taskList.value.filter((task) => !task.completed))
+const completedTasks = computed(() => taskList.value.filter((task) => task.completed))
+
+const deleteTodo = (taskId) => {
+  let index
+  for (let i = 0; i < taskList.value.length; i++) {
+    if (taskList.value[i].id === taskId) {
+      index = i
+      break
+    }
+  }
+  if (index !== undefined) {
+    taskList.value.splice(index, 1)
+  }
+}
 
 onMounted(async () => {
   try {
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="todo-app__task task" v-for="task in props.taskList" :key="task.id">
+    <div>
+      <input type="checkbox" class="task__checkbox" v-if="!task.completed" />
+      <span>{{ task.todo }}</span>
+      <input type="text" class="task__input" v-if="!task.completed" />
+    </div>
+    <div>
+      <button class="task__button" v-if="!task.completed">Edit</button>
+      <button class="task__button" @click="deleteTask(task.id)">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  taskList: {
+    type: Array
+  }
+})
+const emits = defineEmits(['DeleteTask'])
+
+const deleteTask = (taskId) => {
+  emits('DeleteTask', taskId)
+}
+</script>
+
+<style>
+.todo-app__task {
+  display: flex;
+}
+</style>
